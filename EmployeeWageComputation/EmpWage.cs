1338100416f9f0280df732002e2b3f31299ce315
pavanyalala <pavanyalala4508@gmail.com>
2021-07-20 21:39:53 +0530

    [pavan]: Part Time Wage is initialized

--- EmployeeWageComputation/EmpWage.cs
+++ EmployeeWageComputation/EmpWage.cs
@@ -9,6 +9,7 @@ namespace EmployeeWageComputation
         //static variables
         const int WAGE_PER_HR = 20;
         const int FULL_TIME_HR = 8;
+        const int PART_TIME_HR = 4;
         public void WageComputation()//method to calculate employee wage
         {
             Random random = new Random();
@@ -20,7 +21,7 @@ namespace EmployeeWageComputation
             }
             else
             {
-                int total = WAGE_PER_HR * 0;
+                int total = WAGE_PER_HR * PART_TIME_HR;
                 Console.WriteLine("Employee Wage :" + total);
             }
         }
