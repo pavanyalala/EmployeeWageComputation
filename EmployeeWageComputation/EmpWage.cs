b32a7faf38595b83decd5f53c1e98f677435e83f
pavanyalala <pavanyalala4508@gmail.com>
2021-07-20 22:03:39 +0530

    [pavan]:Employee wage initialized using Switch case

--- EmployeeWageComputation/EmpWage.cs
+++ EmployeeWageComputation/EmpWage.cs
@@ -8,22 +8,26 @@ namespace EmployeeWageComputation
     {
         //static variables
         const int WAGE_PER_HR = 20;
-        const int FULL_TIME_HR = 8;
-        const int PART_TIME_HR = 4;
+        const int FULL_TIME = 1;
+        const int PART_TIME=2;
         public void WageComputation()//method to calculate employee wage
         {
+            int empHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == 0)
+            int empCheck = random.Next(0,3);
+            switch (empCheck)
             {
-                int total = WAGE_PER_HR * FULL_TIME_HR;
-                Console.WriteLine("Employee Wage :" + total);
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                int total = WAGE_PER_HR * PART_TIME_HR;
-                Console.WriteLine("Employee Wage :" + total);
-            }
+            Console.WriteLine("Employee Wage :" + empHrs*WAGE_PER_HR);
         }
     }
 }
