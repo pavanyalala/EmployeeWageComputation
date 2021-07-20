9199236f3cf61be3376675205d356aef6a448601
pavanyalala <pavanyalala4508@gmail.com>
2021-07-20 22:28:47 +0530

    [pavan]: Employee Monthly Wage is refactored

--- EmployeeWageComputation/EmpWage.cs
+++ EmployeeWageComputation/EmpWage.cs
@@ -10,25 +10,35 @@ namespace EmployeeWageComputation
         const int WAGE_PER_HR = 20;
         const int FULL_TIME = 1;
         const int PART_TIME=2;
-        const int WAGE_PER_MONTH = 20;
+        const int WORKING_DAYS_IN_MONTH = 20;
         public void WageComputation()//method to calculate employee wage
         {
             int empHrs = 0;
+            int dailyWage = 0;
+            int totalEmpWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,3);
-            switch (empCheck)
+            for (int day = 0; day < WORKING_DAYS_IN_MONTH; day++)
             {
-                case FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+
+                dailyWage = empHrs * WAGE_PER_HR;
+                totalEmpWage = totalEmpWage + dailyWage;
             }
-            Console.WriteLine("Employee Wage :" + (empHrs*WAGE_PER_HR)*WAGE_PER_MONTH);
+            Console.WriteLine("Employee Monthly Wage :"+totalEmpWage);
         }
     }
 }
